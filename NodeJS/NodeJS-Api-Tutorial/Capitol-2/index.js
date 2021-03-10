673499db059f919ef4c255926245294a34dbc0b1
Aioanei Alexandru <aioanei_alexandruSV@yahoo.com>
2021-03-11 01:10:53 +0200

    gata pe azi cap2

--- NodeJS/NodeJS-Api-Tutorial/Capitol-2/index.js
+++ NodeJS/NodeJS-Api-Tutorial/Capitol-2/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 // folosim routes
 const { getPosts } = require('./routes/post');
+
+// poate intra in loop
+const myOwnMiddle = (req, res, next) => {
+    console.log('middleware here!');
+    next();
+};
+
+// middleware
+app.use(myOwnMiddle);
+app.use(morgan("dev"));
 
 app.get('/', getPosts);
 
